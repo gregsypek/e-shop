4ec87bb3c76e139c65dd080e2e34d15a323dc4b4
gregsypek <47268138+gregsypek@users.noreply.github.com>
2024-01-04 18:39:21 +0100

    create banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/nav/HomeBanner";
+import { products } from "./utils/products";
 
 export default function Home() {
-  return (
-    <div>E-shop</div>
-  )
+	return (
+		<div className="p-8">
+			<Container>
+        <div>
+          <HomeBanner />
+        </div>
+        <div>
+          {products.map((product: any) => {
+            return <div key={product.id}>{product.name}</div>
+          })}
+        </div>
+			</Container>
+		</div>
+	);
 }
