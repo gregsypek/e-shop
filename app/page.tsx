6dc5f93de37696a69a5c33455827bebeb2b24e37
gregsypek <47268138+gregsypek@users.noreply.github.com>
2024-01-04 20:39:18 +0100

    create product card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Container from "./components/Container";
 import HomeBanner from "./components/nav/HomeBanner";
+import ProductCard from "./components/products/ProductCard";
 import { products } from "./utils/products";
+import { truncateText } from "./utils/truncateText";
 
 export default function Home() {
 	return (
@@ -9,9 +11,10 @@ export default function Home() {
         <div>
           <HomeBanner />
         </div>
-        <div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {products.map((product: any) => {
-            return <div key={product.id}>{product.name}</div>
+            return <ProductCard key={product.id}data={product} />
+      
           })}
         </div>
 			</Container>
---
+++ notes.md
@@ -0,0 +1,2 @@
+add to ntext.config.js - firebasestorage.googleapis.com
+npm install @mui/material @emotion/react @emotion/styled
