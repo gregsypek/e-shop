fc833fddd939f59794005dd8911d9e457cb67b90
gregsypek <47268138+gregsypek@users.noreply.github.com>
2024-01-07 16:14:47 +0100

    add user menu to navbar

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,23 +1,25 @@
 import Link from "next/link";
 import Container from "../Container";
 import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
 
 const NavBar = () => {
 	return (
 		<div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm ">
-      <div className="py-4 border-b-[1px]" >
-        <Container>
-          <div className="flex items-center justify-between gap-3 md:gap-0">
-            <Link className="font-bold text-2xl" href="/">E-shop</Link>
-            <div className="hidden md:block">Search</div>
-            <div className="flex items-center gap-8">
-        <CartCount />
-              <div>UserMenu</div>
-            </div>
-          </div>
-        </Container>
-      </div>        
-      
+			<div className="py-4 border-b-[1px]">
+				<Container>
+					<div className="flex items-center justify-between gap-3 md:gap-0">
+						<Link className="font-bold text-2xl" href="/">
+							E-shop
+						</Link>
+						<div className="hidden md:block">Search</div>
+						<div className="flex items-center gap-8">
+							<CartCount />
+							<UserMenu />
+						</div>
+					</div>
+				</Container>
+			</div>
 		</div>
 	);
 };
