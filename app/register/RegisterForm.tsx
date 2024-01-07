dd4aa7133a884b6cb856b6569d6bcc3de24583c9
gregsypek <47268138+gregsypek@users.noreply.github.com>
2024-01-07 15:32:38 +0100

    add authentication in Next Auth

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,30 +7,62 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
+	const router = useRouter();
 	const [isLoading, setIsLoading] = useState(false);
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
 	} = useForm<FieldValues>({
-    defaultValues:{
-      name:"", email:"", password:""
-    }
-  });
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+		},
+	});
 
-  const onSubmit:SubmitHandler<FieldValues> = (data) => {
-    console.log("🚀 ~ file: RegisterForm.tsx:22 ~ RegisterForm ~ data:", data)
-    return {
-  }
-setIsLoading(true)
-  }
+	const onSubmit: SubmitHandler<FieldValues> = (data) => {
+		setIsLoading(true);
+
+		axios
+			.post("/api/register", data)
+			.then(() => {
+				toast.success("Account crated");
+
+				signIn("credentials", {
+					email: data.email,
+					password: data.password,
+					redirect: false,
+				}).then((callback) => {
+					if (callback?.ok) {
+						router.push("/cart");
+						router.refresh();
+						toast.success("Logged In");
+					}
+					if (callback?.error) {
+						toast.error(callback.error);
+					}
+				});
+			})
+			.catch(() => toast.error("Something went wrong"))
+			.finally(() => setIsLoading(false));
+	};
 
 	return (
 		<>
 			<Heading title="Sign up for E-Shop" />
-      <Button  outline label="Sign up with Google" icon={AiOutlineGoogle} onClick={()=>{}}/>
+			<Button
+				outline
+				label="Sign up with Google"
+				icon={AiOutlineGoogle}
+				onClick={() => {}}
+			/>
 			<hr className="bg-slate-300 w-full h-px" />
 			<Input
 				id="name"
@@ -55,10 +87,18 @@ setIsLoading(true)
 				register={register}
 				errors={errors}
 				required
-        type="password"
+				type="password"
 			/>
-      <Button  label={isLoading ? "Loading" : "Sign Up"} onClick={handleSubmit(onSubmit)}/>
-      <p className="text-sm">Already have an account? <Link className="underline" href='/login'>Log in</Link></p>
+			<Button
+				label={isLoading ? "Loading" : "Sign Up"}
+				onClick={handleSubmit(onSubmit)}
+			/>
+			<p className="text-sm">
+				Already have an account?{" "}
+				<Link className="underline" href="/login">
+					Log in
+				</Link>
+			</p>
 		</>
 	);
 };
